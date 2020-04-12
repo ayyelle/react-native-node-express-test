690db824eb1d5f4e6de9efdb7a167ceab4369f4e
Anna Leong <awyleong@interchange.ubc.ca>
2020-04-12 11:26:33 -0700

    Testing call to external API

--- client/AwesomeProjectClient/App.js
+++ client/AwesomeProjectClient/App.js
@@ -1,11 +1,11 @@
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
 
 import React, { Component } from "react";
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { apiResponse: "" };
+        this.state = { apiResponse: "", movies: []};
     }
 
     callAPI() {
@@ -15,14 +15,32 @@ class App extends Component {
             .catch(err => err);
     }
 
+    callMoviesAPI() {
+        fetch("https://reactnative.dev/movies.json")
+            .then(res => res.json())
+            .then((json) => this.setState({ movies: json.movies }))
+            .catch(err => err);
+    }
+
     componentDidMount() {
         this.callAPI();
+        this.callMoviesAPI();
     }
 
     render() {
         return (
           <View style={styles.container}>
-      <Text>Data from api: {this.state.apiResponse}</Text>
+      <Text>Data from server api: {this.state.apiResponse}</Text>
+      <View>
+      <Text>Data from external API: </Text>
+        <FlatList
+            data={this.state.movies}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text>{item.title}, {item.releaseYear}</Text>
+            )}
+          />
+      </View>
     </View>
         );
     }
